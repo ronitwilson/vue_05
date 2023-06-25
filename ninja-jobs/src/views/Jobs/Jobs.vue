a0d7e205f8857170ccea37fd041fbe78dc601d4b
ronit <ronit.study@gmail.com>
2023-06-25 16:20:02 +0000

    add fetch from json server

--- ninja-jobs/src/views/Jobs/Jobs.vue
+++ ninja-jobs/src/views/Jobs/Jobs.vue
@@ -2,7 +2,7 @@
   <h1>Jobs</h1>
   <div v-for="job in jobs" :key="job.id" class="job">
     <router-link :to="{name: 'JobDetails', params: { id: job.id }}">
-      <h2>{{ job.title }}</h2>
+      <h2>{{ job.title }} {{ job.id }}</h2>
     </router-link>
   </div>
 </template>
@@ -11,12 +11,22 @@
 export default {
   data() {
     return {
-      jobs: [
-        { title: 'Ninja UX Designer', id: 1, details: 'lorem' },
-        { title: 'Ninja Web Developer', id: 2, details: 'lorem' },
-        { title: 'Ninja Vue Developer', id: 3, details: 'lorem' },
-      ],
+      jobs: [],
     }
+  },
+  methods() {
+
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs').then((res)=> {
+      return(res.json())
+    }).then( (data) => {
+      console.log("debug!!")
+      console.log(data)
+      this.jobs = data
+    }).catch((err) => {
+      console.log(err)
+    })
   }
 }
 </script>
